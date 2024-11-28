ccbaeab10cdb2683156c683930af9aba126af9c0
Ji Hwan <jkim@polygon.technology>
2024-11-28 16:37:25 +0900

    feat: initial template for polycli bridge cmd
    Signed-off-by: Ji Hwan <jkim@polygon.technology>

--- cmd/bridge/bridge.go
+++ cmd/bridge/bridge.go
@@ -38,6 +38,11 @@ var BridgeCmd = &cobra.Command{
 	Long:  "These are low level tools for using the bridge service directly.",
 	Args:  cobra.NoArgs,
 }
+var tokenAddr = common.Address{}
+
+const (
+	mtHeight = 32
+)
 
 //go:embed depositUsage.md
 var depositUsage string
@@ -48,7 +53,46 @@ var depositCmd = &cobra.Command{
 	Args:    cobra.NoArgs,
 	PreRunE: checkDepositArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		deposit()
+		// Convert the string input to uint32
+		destNetworkUint, err := strconv.ParseUint(*bridgeInputArgs.DestNetwork, 10, 32)
+		if err != nil {
+			fmt.Println("Error parsing input:", err)
+			return nil
+		}
+
+		// Since ParseUint returns an uint64, cast it to uint32
+		destNetwork32 := uint32(destNetworkUint)
+
+		// Create a new big.Int
+		amount := new(big.Int)
+
+		// Convert the hexadecimal string to big.Int
+		_, success := amount.SetString(*bridgeInputArgs.FundsToSend, 10)
+		if !success {
+			fmt.Println("Error: Invalid string format for big.Int")
+			return nil
+		}
+
+		ctx := context.Background()
+		client, err := utils.NewClient(ctx, *bridgeInputArgs.OriginNetworkURL, common.HexToAddress(*bridgeInputArgs.OriginBridgeAddr))
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		auth, err := client.GetSigner(ctx, *bridgeInputArgs.OriginAccHexPrivateKey)
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		emptyAddr := common.Address{}
+		if tokenAddr == emptyAddr {
+			auth.Value = amount
+		}
+		destAddr := common.HexToAddress(*bridgeInputArgs.OriginAccHexAddress)
+		log.Info("Sending bridge tx...")
+		err = client.SendBridgeAsset(ctx, tokenAddr, amount, destNetwork32, &destAddr, []byte{}, auth)
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		log.Info("Success!")
 		return nil
 	},
 }
@@ -62,149 +106,68 @@ var claimCmd = &cobra.Command{
 	Args:    cobra.NoArgs,
 	PreRunE: checkClaimArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		claim()
+		ctx := context.Background()
+		c, err := utils.NewClient(ctx, *bridgeInputArgs.DestNetworkURL, common.HexToAddress(*bridgeInputArgs.DestBridgeAddr))
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		auth, err := c.GetSigner(ctx, *bridgeInputArgs.DestAccHexPrivateKey)
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+
+		// Get Claim data
+		cfg := clientUtils.Config{
+			L1NodeURL:    *bridgeInputArgs.DestNetworkURL,
+			L2NodeURL:    *bridgeInputArgs.DestNetworkURL,
+			BridgeURL:    *bridgeInputArgs.BridgeURL,
+			L2BridgeAddr: common.HexToAddress(*bridgeInputArgs.DestBridgeAddr),
+		}
+		client, err := clientUtils.NewClient(ctx, cfg)
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		deposits, _, err := client.GetBridges(*bridgeInputArgs.DestAccHexAddress, 0, 10) //nolint
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		bridgeData := deposits[0]
+		proof, err := client.GetMerkleProof(deposits[0].NetworkId, deposits[0].DepositCnt)
+		if err != nil {
+			log.Fatal("error: ", err)
+		}
+		log.Debug("bridge: ", bridgeData)
+		log.Debug("mainnetExitRoot: ", proof.MainExitRoot)
+		log.Debug("rollupExitRoot: ", proof.RollupExitRoot)
+
+		var smtProof, smtRollupProof [mtHeight][32]byte
+		for i := 0; i < len(proof.MerkleProof); i++ {
+			log.Debug("smtProof: ", proof.MerkleProof[i])
+			smtProof[i] = common.HexToHash(proof.MerkleProof[i])
+			log.Debug("smtRollupProof: ", proof.RollupMerkleProof[i])
+			smtRollupProof[i] = common.HexToHash(proof.RollupMerkleProof[i])
+		}
+		globalExitRoot := &etherman.GlobalExitRoot{
+			ExitRoots: []common.Hash{common.HexToHash(proof.MainExitRoot), common.HexToHash(proof.RollupExitRoot)},
+		}
+		log.Info("Sending claim tx...")
+		err = c.SendClaim(ctx, bridgeData, smtProof, smtRollupProof, globalExitRoot, auth)
+		if err != nil {
+			log.Fatal("error: ", err)
+		}
+		log.Info("Success!")
+		balance, err := c.Client.BalanceAt(ctx, common.HexToAddress(*bridgeInputArgs.DestAccHexAddress), nil)
+		if err != nil {
+			log.Fatal("error getting balance: ", err)
+		}
+		log.Info("L2 balance: ", balance)
 		return nil
 	},
-}
-
-var tokenAddr = common.Address{}
-
-func deposit() {
-	// // Check if sufficient arguments are provided
-	// if len(os.Args) < 7 {
-	// 	fmt.Println("Usage: <program_name> <originBridgeAddr> <originAccHexAddress> <originAccHexPrivateKey> <originNetworkURL> <fundsToSend> <destNetwork>")
-	// 	return
-	// }
-	// // Get the name and age from the command-line arguments
-	// originBridgeAddr := os.Args[1]
-	// originAccHexAddress := os.Args[2]
-	// originAccHexPrivateKey := os.Args[3]
-	// originNetworkURL := os.Args[4]
-	// fundsToSend := os.Args[5]
-	// destNetwork := os.Args[6]
-
-	// Convert the string input to uint32
-	destNetworkUint, err := strconv.ParseUint(destNetwork, 10, 32)
-	if err != nil {
-		fmt.Println("Error parsing input:", err)
-		return
-	}
-
-	// Since ParseUint returns an uint64, cast it to uint32
-	destNetwork32 := uint32(destNetworkUint)
-
-	// Create a new big.Int
-	amount := new(big.Int)
-
-	// Convert the hexadecimal string to big.Int
-	_, success := amount.SetString(fundsToSend, 10)
-	if !success {
-		fmt.Println("Error: Invalid string format for big.Int")
-		return
-	}
-
-	ctx := context.Background()
-	client, err := utils.NewClient(ctx, originNetworkURL, common.HexToAddress(originBridgeAddr))
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	auth, err := client.GetSigner(ctx, originAccHexPrivateKey)
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	emptyAddr := common.Address{}
-	if tokenAddr == emptyAddr {
-		auth.Value = amount
-	}
-	destAddr := common.HexToAddress(originAccHexAddress)
-	log.Info("Sending bridge tx...")
-	err = client.SendBridgeAsset(ctx, tokenAddr, amount, destNetwork32, &destAddr, []byte{}, auth)
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	log.Info("Success!")
-}
-
-const (
-	mtHeight = 32
-)
-
-func claim() {
-	// // Check if sufficient arguments are provided
-	// if len(os.Args) < 6 {
-	// 	fmt.Println("Usage: <program_name> <destBridgeAddr> <destAccHexAddress> <destAccHexPrivateKey> <destNetworkURL> <bridgeURL>")
-	// 	return
-	// }
-	// // Get the name and age from the command-line arguments
-	// destBridgeAddr := os.Args[1]
-	// destAccHexAddress := os.Args[2]
-	// destAccHexPrivateKey := os.Args[3]
-	// destNetworkURL := os.Args[4]
-	// bridgeURL := os.Args[5]
-
-	ctx := context.Background()
-	c, err := utils.NewClient(ctx, destNetworkURL, common.HexToAddress(destBridgeAddr))
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	auth, err := c.GetSigner(ctx, destAccHexPrivateKey)
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-
-	// Get Claim data
-	cfg := clientUtils.Config{
-		L1NodeURL:    destNetworkURL,
-		L2NodeURL:    destNetworkURL,
-		BridgeURL:    bridgeURL,
-		L2BridgeAddr: common.HexToAddress(destBridgeAddr),
-	}
-	client, err := clientUtils.NewClient(ctx, cfg)
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	deposits, _, err := client.GetBridges(destAccHexAddress, 0, 10) //nolint
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	bridgeData := deposits[0]
-	proof, err := client.GetMerkleProof(deposits[0].NetworkId, deposits[0].DepositCnt)
-	if err != nil {
-		log.Fatal("error: ", err)
-	}
-	log.Debug("bridge: ", bridgeData)
-	log.Debug("mainnetExitRoot: ", proof.MainExitRoot)
-	log.Debug("rollupExitRoot: ", proof.RollupExitRoot)
-
-	var smtProof, smtRollupProof [mtHeight][32]byte
-	for i := 0; i < len(proof.MerkleProof); i++ {
-		log.Debug("smtProof: ", proof.MerkleProof[i])
-		smtProof[i] = common.HexToHash(proof.MerkleProof[i])
-		log.Debug("smtRollupProof: ", proof.RollupMerkleProof[i])
-		smtRollupProof[i] = common.HexToHash(proof.RollupMerkleProof[i])
-	}
-	globalExitRoot := &etherman.GlobalExitRoot{
-		ExitRoots: []common.Hash{common.HexToHash(proof.MainExitRoot), common.HexToHash(proof.RollupExitRoot)},
-	}
-	log.Info("Sending claim tx...")
-	err = c.SendClaim(ctx, bridgeData, smtProof, smtRollupProof, globalExitRoot, auth)
-	if err != nil {
-		log.Fatal("error: ", err)
-	}
-	log.Info("Success!")
-	balance, err := c.Client.BalanceAt(ctx, common.HexToAddress(destAccHexAddress), nil)
-	if err != nil {
-		log.Fatal("error getting balance: ", err)
-	}
-	log.Info("L2 balance: ", balance)
 }
 
 func checkDepositArgs(cmd *cobra.Command, args []string) error {
 	// if *ulxlyInputArgs.DepositBridgeAddress == "" {
 	// 	return fmt.Errorf("please provide the bridge address")
-	// }
-	// if *ulxlyInputArgs.DepositGasLimit < 130000 && *ulxlyInputArgs.DepositGasLimit != 0 {
-	// 	return fmt.Errorf("the gas limit may be too low for the transaction to pass")
 	// }
 	return nil
 }
@@ -212,9 +175,6 @@ func checkDepositArgs(cmd *cobra.Command, args []string) error {
 func checkClaimArgs(cmd *cobra.Command, args []string) error {
 	// if *ulxlyInputArgs.ClaimGasLimit < 150000 && *ulxlyInputArgs.ClaimGasLimit != 0 {
 	// 	return fmt.Errorf("the gas limit may be too low for the transaction to pass")
-	// }
-	// if *ulxlyInputArgs.ClaimMessage && *ulxlyInputArgs.ClaimWETH {
-	// 	return fmt.Errorf("choose a single claim mode (asset, message, or WETH)")
 	// }
 	return nil
 }
@@ -233,7 +193,7 @@ func init() {
 	bridgeInputArgs.DestBridgeAddr = claimCmd.PersistentFlags().String("destination-bridge-address", "0xd8886e9D827218a02B8C04323b5550f2F36BC8d5", "")
 	bridgeInputArgs.DestAccHexAddress = claimCmd.PersistentFlags().String("destination-account-address", "0xE34aaF64b29273B7D567FCFc40544c014EEe9970", "")
 	bridgeInputArgs.DestAccHexPrivateKey = claimCmd.PersistentFlags().String("destination-account-private-key", "0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625", "")
-	bridgeInputArgs.DestNetworkURL = claimCmd.PersistentFlags().String("destination-network-url", "0", "")
+	bridgeInputArgs.DestNetworkURL = claimCmd.PersistentFlags().String("destination-network-url", "http://127.0.0.1:8545", "")
 	bridgeInputArgs.BridgeURL = claimCmd.PersistentFlags().String("bridge-url", "http://127.0.0.1:8080", "")
 
 }
